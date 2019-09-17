a79124ac5efa2695d8693f591e13189b2baf9174
Seth Dousman <sdousman3@gmail.com>
2019-09-17 18:15:33 -0400

    crystal game 3.0

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,35 +1,57 @@
 
 
 var randomNumber;
-var loss;
-var win;
+var loss = 0;
+var win = 0;
+var previous = 0;
 
+var startGame = function(){
+    $(".crystals").empty();
 
 randomNumber = Math.floor(Math.random() * 101) + 19;
 
-
 $("#result").html('Random Result: ' + randomNumber)
-
 
 for(var i = 0; i < 4; i++) {
 
     var random = Math.floor(Math.random() * 11) +1;
+
+    
     
     var crystal = $("<div>");
-    crystal.attr({
+        crystal.attr({
         "class":'crystal',
         "data-random": random
     });
-
+    crystal.html(random);
     $(".crystals").append(crystal);
 
 }
+}
+startGame();
 
-$(".crystal").on('click', function() {
+
+
+
+$(document).on('click',".crystal", function() {
 
     var num = parseInt($(this).attr("data-random"));
-    var result = num + 5;
-    console.log(result);
+    previous+=num;
+   
+  
+    if( previous > randomNumber){
+        loss--;
+    $("#loss").html(loss);
+    previous = 0;
+    startGame();
+    } else if (previous === randomNumber){
+        win++;
+    $("#win").html(win);
+    previous = 0;
+    startGame();
+
+    
+    }
 
 
 
